258ad1815b3cb62d34cda37041c0d3fd51293d42
The Count <itapuc7@gmail.com>
2024-01-11 13:29:07 -0500

    Got screenRecorder to work
    Still have to make sure that the audio records correctly.

--- audio.py
+++ audio.py
@@ -3,7 +3,7 @@ import soundfile as sf
 
 OUTPUT_FILE_NAME = "out.wav"    # file name.
 SAMPLE_RATE = 48000              # [Hz]. sampling rate.
-RECORD_SEC = 10                  # [sec]. duration recording audio.
+RECORD_SEC = 30                  # [sec]. duration recording audio.
 
 with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE) as mic:
     # record audio with loopback from default speaker.
@@ -11,3 +11,25 @@ with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True)
     
     # change "data=data[:, 0]" to "data=data", if you would like to write audio as multiple-channels.
     sf.write(file=OUTPUT_FILE_NAME, data=data[:, 0], samplerate=SAMPLE_RATE)
+
+# from moviepy.editor import VideoFileClip, AudioFileClip
+
+# def combine_audio_video(video_path, audio_path, output_path):
+#     # Load video clip
+#     video_clip = VideoFileClip(video_path)
+    
+#     # Load audio clip
+#     audio_clip = AudioFileClip(audio_path)
+    
+#     # Set video clip audio to the loaded audio clip
+#     video_clip = video_clip.set_audio(audio_clip)
+    
+#     # Write the result to a new file
+#     video_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
+
+# if __name__ == "__main__":
+#     video_path = "video.mp4"
+#     audio_path = "out.wav"
+#     output_path = "output_video.mp4"
+    
+#     combine_audio_video(video_path, audio_path, output_path)
--- screenRecorder.js
+++ screenRecorder.js
@@ -1,52 +1,44 @@
-const { desktopCapturer } = require('electron');
-const fs = require('fs');
-const moment = require('moment');
-
-// Get today's date.
-let now = new Date();
-
-// Set the recording time.
-let recordDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0);
-
-let duration = 10*1000; // Recording for 10 seconds.
-
-// Initiate timer
-setTimeout(startRecording, 100);
+require('dotenv').config()
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+const { PuppeteerScreenRecorder } = require('puppeteer-screen-recorder');
+const { exec } = require('child_process');
 
 
-function startRecording() {  
-  desktopCapturer.getSources({types: ['screen']}).then(async sources => {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        mandatory: {
-          chromeMediaSource: 'desktop',
-          chromeMediaSourceId: sources[0].id,
-        }
-      }
-    });
+const wait = ms => new Promise(res => setTimeout(res, ms));
 
-    const options = { mimeType: 'video/webm; codecs=vp9' };
-    const mediaRecorder = new MediaRecorder(stream, options);
-    mediaRecorder.ondataavailable = handleDataAvailable;
-    mediaRecorder.onstop = handleStop;
+(async () => {
+  const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized'] });
 
-    // Starts recording
-    mediaRecorder.start();
+  const page = await browser.newPage();
+  await page.goto('https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fclassroom.google.com%2F&emr=1&followup=https%3A%2F%2Fclassroom.google.com%2F&ifkv=ASKXGp38XQD342-5MlElrLTgg7IAgglt6UixzE8dNhNGh8YoEeZdidrunhedzyLyId_408CDPPkg&passive=1209600&service=classroom&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-720719761%3A1704937311118373&theme=glif');
+  await wait(2000);
 
-    // Stops recording after 10 seconds
-    setTimeout(() => mediaRecorder.stop(), duration);  
-  }).catch(err => {
-    console.error('Failed to get sources: ' + err);
+  await page.type('input[type^="email"]', process.env.EMAIL);
+  await page.keyboard.press('Enter');
+
+  await wait(2000);
+  
+  await page.type('input[type^="password"]', process.env.PASSWORD);
+  await page.keyboard.press('Enter');
+
+
+
+  const recorder = new PuppeteerScreenRecorder(page);
+  await recorder.start('./video/simple.mp4')
+  
+  exec('python audio.py', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing the Python script: ${error}`);
+      return;
+    }
+
+    console.log(`Python script output:\n${stdout}`);
   });
-}
+  
+  await wait(30000);
 
-function handleDataAvailable(e) {
-  // Append the data to the file.
-  fs.appendFileSync('screencapture.webm', e.data);
-}
-
-function handleStop(e) {
-  console.log('Recording finished.');
-}
-
+  await recorder.stop();
+  await browser.close();
+})();
