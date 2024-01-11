3e689d295823c93c5b000066d74489cc3367b4e1
Justin T <35670603+ConteDeStGermain@users.noreply.github.com>
2024-01-11 15:00:49 -0500

    Done
    The app is working and in sync

--- screenRecorder.js
+++ screenRecorder.js
@@ -4,6 +4,7 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 const { PuppeteerScreenRecorder } = require('puppeteer-screen-recorder');
 const { exec } = require('child_process');
+const { resolve } = require('path');
 
 
 const wait = ms => new Promise(res => setTimeout(res, ms));
@@ -12,22 +13,20 @@ const wait = ms => new Promise(res => setTimeout(res, ms));
   const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized'] });
 
   const page = await browser.newPage();
-  await page.goto('https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fclassroom.google.com%2F&emr=1&followup=https%3A%2F%2Fclassroom.google.com%2F&ifkv=ASKXGp38XQD342-5MlElrLTgg7IAgglt6UixzE8dNhNGh8YoEeZdidrunhedzyLyId_408CDPPkg&passive=1209600&service=classroom&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-720719761%3A1704937311118373&theme=glif');
-  await wait(2000);
+  // await page.goto('https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fclassroom.google.com%2F&emr=1&followup=https%3A%2F%2Fclassroom.google.com%2F&ifkv=ASKXGp38XQD342-5MlElrLTgg7IAgglt6UixzE8dNhNGh8YoEeZdidrunhedzyLyId_408CDPPkg&passive=1209600&service=classroom&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-720719761%3A1704937311118373&theme=glif');
+  await page.goto('https://www.youtube.com/watch?v=3VfL1PH8D4A&ab_channel=FunkyScott47');
+  await wait(10000);
 
-  await page.type('input[type^="email"]', process.env.EMAIL);
-  await page.keyboard.press('Enter');
+  // await page.type('input[type^="email"]', process.env.EMAIL);
+  // await page.keyboard.press('Enter');
 
-  await wait(2000);
+  // await wait(2000);
   
-  await page.type('input[type^="password"]', process.env.PASSWORD);
-  await page.keyboard.press('Enter');
+  // await page.type('input[type^="password"]', process.env.PASSWORD);
+  // await page.keyboard.press('Enter');
+  const recorder = new PuppeteerScreenRecorder(page, {fps: 45});  
 
-
-
-  const recorder = new PuppeteerScreenRecorder(page);
-  await recorder.start('./video/simple.mp4')
-  
+ const audioPromise = new Promise((resolve) => {
   exec('python audio.py', (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing the Python script: ${error}`);
@@ -35,10 +34,33 @@ const wait = ms => new Promise(res => setTimeout(res, ms));
     }
 
     console.log(`Python script output:\n${stdout}`);
+    resolve();
+  });
+ })
+
+  // exec('python audio.py', (error, stdout, stderr) => {
+  //   if (error) {
+  //     console.error(`Error executing the Python script: ${error}`);
+  //     return;
+  //   }
+
+  //   console.log(`Python script output:\n${stdout}`);
+  // });
+  
+  await recorder.start('./video/simple.mp4')
+  await audioPromise;
+  await recorder.stop();
+
+
+  await browser.close();
+  exec('python combine.py', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing the Python script: ${error}`);
+      return;
+    }
+
+    console.log(`Python script output:\n${stdout}`);
+    
   });
   
-  await wait(30000);
-
-  await recorder.stop();
-  await browser.close();
 })();
